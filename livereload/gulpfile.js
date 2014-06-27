adf057dfd19e7c6b09cad7cc3e64d2c5f223d723
turtlemt <turtlemt@gmail.com>
2014-06-27 15:56:58 +0800

    Fix bug

--- livereload/gulpfile.js
+++ livereload/gulpfile.js
@@ -1,6 +1,8 @@
 var gulp        = require('gulp');
 var connect     = require('gulp-connect');
 
+var public_path = './public';
+
 // listen
 gulp.task('connect', function()
 {
@@ -11,18 +13,18 @@ gulp.task('connect', function()
 });
 
 gulp.task('html', function () {
-    gulp.src('./public/*.html')
+    gulp.src( public_path + '/*.html')
         .pipe(connect.reload());
 });
 
 gulp.task('css', function () {
-    gulp.src('./public/*.css')
+    gulp.src( public_path + '/*.css')
         .pipe(connect.reload());
 });
 
 gulp.task('watch', function () {
-    gulp.watch(['*.*'], ['html']);
-    gulp.watch(['*.*'], ['css']);
+    gulp.watch([ public_path + '/*.*'], ['html']);
+    gulp.watch([ public_path + '/*.*'], ['css']);
 });
 
 /* --------------------------------------------------------------------------------
